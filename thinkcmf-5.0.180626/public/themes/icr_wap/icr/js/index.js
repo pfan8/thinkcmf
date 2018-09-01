67c4c650069fc65148e13c40367ac25dde309eec
pfan8 <fqwgc8@outlook.com>
2018-09-02 00:47:36 +0800

    mobile site

--- thinkcmf-5.0.180626/public/themes/icr_wap/icr/js/index.js
+++ thinkcmf-5.0.180626/public/themes/icr_wap/icr/js/index.js
@@ -15,4 +15,53 @@ $(function(){
             $(this).addClass('fa-square-o')
         }
     });
+    //轮播
+    var page = 1;
+    var slide= function(){
+        var num = $(".slide ul").find("li").length;
+        if(num<=1){
+            clearInterval(slideInterval);
+            return ;
+        }
+        if(page<=num){
+            $(".slide ul").animate({
+                marginLeft:(-(page-1)*7.5)+"rem"
+            })
+            page++;
+        }else{
+            $('.slide ul').css("margin-left",0);
+            page=1;
+        }
+
+    }
+    var slideInterval =setInterval(slide,"2000");
+
+
+    //预约弹窗定义
+    $("[name='yy']").click(function(){
+        var school_name = $(this).data('name');
+        var school_position=$(this).data('position');
+        if($('body').find('#reservation_wd').length){
+            //存在显示
+            $(".wd-title2").html(school_name+"："+school_position);
+            $('#reservation_wd').show();
+        }else{
+            //不存在加载
+
+            $('body').append(reservation_wd_html);
+            $(".wd-title2").html(school_name+"："+school_position);
+            $("#reservation_wd").show();
+        }
+        $(".cease").click(function(){
+            $("#popup").remove();
+        });
+
+    });
+    $("[name='submit_reservation']").click(function(){
+        //在这绑定预约请求
+    });
+    //绑定城市选择
+    $(".head-title,.head-right").click(function(){
+        $(".head-position").toggle();
+    })
 });
